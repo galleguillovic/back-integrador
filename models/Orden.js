ae7d1dd34bd0514e1e67e69deee1fb5a27b39701
Victoria Antonela Galleguillo <vic54galleguillo@gmail.com>
2025-10-29 16:43:18 -0300

    Agregué campos e hice un script de migración para actualizar datos

--- models/Orden.js
+++ models/Orden.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const ordenSchema = new mongoose.Schema({
     destino: { type: String, required: [true, 'El destino es obligatorio']},
     contenido: { type: String, required: [true, 'El contenido es obligatorio']},
+    categoria: { type: String, required: [true, 'La categoria es obligatoria']},
     fecha_creacion: { type: Date, default: Date.now},
-    estado: { type: String, required: [true, 'El estado es obligatorio'], enum: ['Pendiente', 'En tránsito', 'Entregado']}
+    estado: { type: String, required: [true, 'El estado es obligatorio'], enum: ['Pendiente', 'En tránsito', 'Entregado']},
+    repartidor: { type: String, default: null },
+    peso: { type: Number, required: [true, 'El peso es obligatorio'], min: [0, 'El peso debe ser un número mayor o igual a 0 (kg)'] }
 });
 
 module.exports = mongoose.model('Orden', ordenSchema);
