bd9b8b138b23b31d8fb1f8775a738e61ab0aff31
Victoria Antonela Galleguillo <vic54galleguillo@gmail.com>
2025-10-30 14:46:49 -0300

    Nueva act

--- models/Orden.js
+++ models/Orden.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const ordenSchema = new mongoose.Schema({
-    destino: { type: String, required: [true, 'El destino es obligatorio']},
-    contenido: { type: String, required: [true, 'El contenido es obligatorio']},
-    categoria: { type: String, required: [true, 'La categoria es obligatoria']},
+    destino: { type: String, required: [true, 'Destino es obligatorio']},
+    contenido: { type: String, required: [true, 'Contenido es obligatorio']},
     fecha_creacion: { type: Date, default: Date.now},
-    estado: { type: String, required: [true, 'El estado es obligatorio'], enum: ['Pendiente', 'En tránsito', 'Entregado']},
+    estado: { type: String, required: [true, 'Estado es obligatorio'], enum: ['Pendiente', 'En tránsito', 'Entregado']},
     repartidor: { type: String, default: null },
-    peso: { type: Number, required: [true, 'El peso es obligatorio'], min: [0, 'El peso debe ser un número mayor o igual a 0 (kg)'] }
+    categoria: { type: String, required: [true, "Categoría obligatoria"], enum: ['Documentos','Tecnología','Ropa y accesorios','Hogar y decoración','Alimentos y bebidas','Salud y belleza','Otros'] },
+    peso: { type: Number, required: [true, 'Peso(kg) es obligatorio'], min: [0] }
 });
 
 module.exports = mongoose.model('Orden', ordenSchema);
