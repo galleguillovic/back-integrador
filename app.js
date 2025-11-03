eafa29a63e6401ad5a8c4d0e97d5fe523b5e6121
Victoria Antonela Galleguillo <vic54galleguillo@gmail.com>
2025-11-03 15:14:01 -0300

    Mod para el front

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const dbconnect = require('./config/db');
 const ordenesRoutes = require('./routes/ordenes');
+require('dotenv').config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://logisticafront-integrador.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: false, // true para auth con cookies
+  })
+);
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente 🚚'); 
+});
+
 app.use('/ordenes', ordenesRoutes);
 
 dbconnect().then(() => {
-    app.listen(3000, () => console.log("Corriendo en http://localhost:3000"));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
 });
